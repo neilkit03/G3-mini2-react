c9e6bcf5b87048eba89119b6f798bcf591af7848
neilkit03 <neilkit03@gmail.com>
2023-03-16 17:25:11 +0800

    hover:pointer

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,7 +14,23 @@ function services() {
             <h2>Our Services</h2>
           </div>
           <div className='row'>
+<<<<<<< HEAD
             <ServicesItems Title={serviceTitle} Content={serviceContent} />
+=======
+            <div className='col-md-4'>
+              <div className='service-media'>
+                {" "}
+                <img src='img/services/service-1.jpg' alt=' ' />{" "}
+              </div>
+              <div className='service-desc'>
+                <h3>New Home Construction</h3>
+                <p>
+                The first in our list of construction technology trends is 3D printing.
+                 A little over a decade ago, 3D printing emerged as a possible remedy for disaster-stricken regions around the world. 
+                </p>
+              </div>
+            </div>
+>>>>>>> b96021b91933e7c31d2c0ad6826144d41b5f2b9c
             <div className='col-md-4'>
               <div className='service-media'>
                 {" "}
@@ -23,10 +39,8 @@ function services() {
               <div className='service-desc'>
                 <h3>Home Additions</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at. Lorem ipsum dolor sit
-                  amet, consectetur adipiscing elit.
+                If you’re like most people, you spend more time in your home than anywhere else. 
+                So if there’s something about your home that isn’t quite right, it can become problematic. 
                 </p>
               </div>
             </div>
@@ -38,9 +52,8 @@ function services() {
               <div className='service-desc'>
                 <h3>Bathroom Remodels</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at.
+                  Is your home in need of a bathroom remodel? 
+                  Give your bathroom design a boost with a little planning and our inspirational bathroom remodel ideas.
                 </p>
               </div>
             </div>
@@ -54,9 +67,8 @@ function services() {
               <div className='service-desc'>
                 <h3>Kitchen Remodels</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at.
+                For avid cooks, seasoned hosts, and busy families alike, 
+                the kitchen is the center of daily life—and a place where both beauty and function are more important than ever.
                 </p>
               </div>
             </div>
@@ -68,9 +80,9 @@ function services() {
               <div className='service-desc'>
                 <h3>Windows & Doors</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at.
+                Surely, the infrastructural things like the beams, 
+                bricks, and mortars, etc play a significant role, and obviously, 
+                people take great care towards the same. 
                 </p>
               </div>
             </div>
@@ -82,9 +94,8 @@ function services() {
               <div className='service-desc'>
                 <h3>Decks & Porches</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  sed dapibus leo nec ornare diam sedasd commodo nibh ante
-                  facilisis bibendum dolor feugiat at.
+                Modern building codes pay a lot of attention to critical details
+                 such as ledgers and railings, but codes are not always consistently enforced.
                 </p>
               </div>
             </div>
